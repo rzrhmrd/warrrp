07a57b57a4f35ddace51bfa1c92ff9fe681ad07d
rzmmzdh <31274878+rzmmzdh@users.noreply.github.com>
2024-07-14 14:57:09 +0330

    Update warpraw.py

--- warpraw.py
+++ warpraw.py
@@ -7,16 +7,12 @@ import base64
 import pytz
 import json
 
-# Define the CIDR ranges for Warp IPs
+# Define the CIDR ranges for Warp IPv6 IPs
 WARP_CIDR_RANGES = [
-    '162.159.192.0/24',
-    '162.159.193.0/24',
-    '162.159.195.0/24',
-    '162.159.204.0/24',
-    '188.114.96.0/24',
-    '188.114.97.0/24',
-    '188.114.98.0/24',
-    '188.114.99.0/24'
+    '2606:4700:4700::/48',
+    '2606:4700:4701::/48',
+    '2606:4700:4702::/48',
+    '2606:4700:4703::/48'
 ]
 
 # Define paths to the necessary files
@@ -31,11 +27,11 @@ def get_repository_name():
 
 def generate_ip_list():
     """Generate a list of IP addresses from the given CIDR ranges and save to a file."""
-    total_ips = sum(len(list(ipaddress.IPv4Network(cidr))) for cidr in WARP_CIDR_RANGES)
+    total_ips = sum(len(list(ipaddress.IPv6Network(cidr))) for cidr in WARP_CIDR_RANGES)
 
     with open(IP_PATH, 'w') as ip_file:
         for cidr in WARP_CIDR_RANGES:
-            for i, ip_addr in enumerate(ipaddress.IPv4Network(cidr)):
+            for i, ip_addr in enumerate(ipaddress.IPv6Network(cidr)):
                 ip_file.write(str(ip_addr))
                 if i != total_ips - 1:
                     ip_file.write('\n')
