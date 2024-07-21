ac24c8066a7356974eeab4f702d4942393bb8232
rzmmzdh <31274878+rzmmzdh@users.noreply.github.com>
2024-07-22 01:58:55 +0330

    Update warpraw.py

--- warpraw.py
+++ warpraw.py
@@ -1,23 +1,23 @@
 import subprocess, os, datetime, base64, pytz, random
 
 SCRIPT_DIR = os.path.dirname(__file__)
-RESULT_CSV_PATH = os.path.join(SCRIPT_DIR, 'result.csv')
-WARP_PROGRAM_PATH = os.path.join(SCRIPT_DIR, 'bin', 'warp')
+IP_SCAN_RESULTS_PATH = os.path.join(SCRIPT_DIR, 'result.csv')
+WARP_BINARY_PATH = os.path.join(SCRIPT_DIR, 'bin', 'warp')
 
 def get_repository_name():
     return os.path.basename(os.path.dirname(SCRIPT_DIR)).upper()
 
 def run_warp_program():
-    if not os.path.exists(WARP_PROGRAM_PATH):
-        raise RuntimeError(f"Warp binary not found at {WARP_PROGRAM_PATH}")
-    os.chmod(WARP_PROGRAM_PATH, 0o755)
-    process = subprocess.run([WARP_PROGRAM_PATH], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    if not os.path.exists(WARP_BINARY_PATH):
+        raise RuntimeError(f"Warp binary not found at {WARP_BINARY_PATH}")
+    os.chmod(WARP_BINARY_PATH, 0o755)
+    process = subprocess.run([WARP_BINARY_PATH], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     if process.returncode != 0:
         raise RuntimeError("Warp execution failed")
 
 def extract_clean_ips():
     clean_ips = []
-    with open(RESULT_CSV_PATH, 'r') as csv_file:
+    with open(IP_SCAN_RESULTS_PATH, 'r') as csv_file:
         next(csv_file)  # Skip header
         for i, line in enumerate(csv_file):
             clean_ips.append(line.split(',')[0])
@@ -30,7 +30,7 @@ def base64_encode(data):
 
 def get_last_update_time():
     try:
-        creation_time = os.path.getctime(RESULT_CSV_PATH)
+        creation_time = os.path.getctime(IP_SCAN_RESULTS_PATH)
     except OSError as e:
         print(f"Error accessing the result CSV file: {e}")
         return None
@@ -56,9 +56,9 @@ def generate_warp_config(clean_ips, last_update_time):
 
 def clean_up():
     try:
-        os.remove(RESULT_CSV_PATH)
+        os.remove(IP_SCAN_RESULTS_PATH)
     except OSError as e:
-        print(f"Error removing file {RESULT_CSV_PATH}: {e}")
+        print(f"Error removing file {IP_SCAN_RESULTS_PATH}: {e}")
 
 def main():
     run_warp_program()
