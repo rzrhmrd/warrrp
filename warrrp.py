1fa418123e9a5f8612070ffe0c9672538ebeecf7
rzrhmrd <31274878+rzrhmrd@users.noreply.github.com>
2024-10-11 02:34:22 +0330

    Update warrrp.py

--- warrrp.py
+++ warrrp.py
@@ -19,34 +19,34 @@ def run_warp_server_scanner():
     """Runs the Warp server scanner binary."""
     if not os.path.exists(WARP_SERVER_SCANNER_PATH):
         raise RuntimeError(f"Warp binary not found at {WARP_SERVER_SCANNER_PATH}")
-    
+
     os.chmod(WARP_SERVER_SCANNER_PATH, 0o755)
     process = subprocess.run([WARP_SERVER_SCANNER_PATH], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    
+
     if process.returncode != 0:
         raise RuntimeError("Warp execution failed")
 
 def extract_top_two_servers():
-    """Extracts the top two server addresses from the CSV results."""
+    """Extracts the top two server addresses with ports from the CSV results."""
     top_servers = []
-    
+
     try:
         with open(SERVER_SCAN_RESULTS_PATH, 'r') as csv_file:
             reader = csv.reader(csv_file)
             next(reader)  # Skip header
-            
+
             for row in reader:
-                server_address = row[0].split(':')[0]  # Extract only the server address
+                server_address = row[0]
                 top_servers.append(server_address)
-                
+
                 if len(top_servers) == 2:
                     break
-    
+
     except FileNotFoundError:
         raise RuntimeError(f"CSV file not found at {SERVER_SCAN_RESULTS_PATH}")
     except Exception as e:
         raise RuntimeError(f"Error reading CSV file: {e}")
-    
+
     return top_servers
 
 def base64_encode(data):
@@ -60,7 +60,7 @@ def get_last_update_time():
     except OSError as e:
         print(f"Error accessing the result CSV file: {e}")
         return None
-    
+
     tehran_tz = pytz.timezone('Asia/Tehran')
     local_time = datetime.datetime.fromtimestamp(creation_time, tehran_tz)
     return local_time.strftime("%Y-%m-%d %H:%M") + " Tehran, Iran Time"
@@ -68,11 +68,11 @@ def get_last_update_time():
 def generate_warp_config(top_servers, last_update_time):
     """Generates and writes the Warp configuration based on the top servers and last update time."""
     plus_key = os.getenv('PLUS_KEY')
-    
+
     warp_config = (
         f"warp://{top_servers[0]}?ifp=20-35&ifps=5-35&ifpd=5-35&ifpm=m4#IR&&detour=warp://{top_servers[1]}#DE"
     )
-    
+
     warp_hiddify_config = (
         f"#profile-title: base64:{base64_encode(get_repository_name())}\n"
         f"#profile-update-interval: 1\n"
@@ -81,38 +81,9 @@ def generate_warp_config(top_servers, last_update_time):
         f"#last-update: {last_update_time}\n"
         f"{warp_config}"
     )
-    
+
     try:
         with open(CONFIG_FILE_PATH, 'w') as config_file:
             config_file.write(base64_encode(warp_hiddify_config))
     except IOError as e:
-        print(f"Error writing to configuration file: {e}")
-
-def clean_up():
-    """Cleans up by removing the result CSV file."""
-    try:
-        os.remove(SERVER_SCAN_RESULTS_PATH)
-    except OSError as e:
-        print(f"Error removing file {SERVER_SCAN_RESULTS_PATH}: {e}")
-
-def main():
-    """Main function to run the warp server scanner and generate the config."""
-    run_warp_server_scanner()
-    top_servers = extract_top_two_servers()
-    
-    if len(top_servers) < 2:
-        print("Error: Not enough servers found.")
-        return
-    
-    last_update_time = get_last_update_time()
-    
-    if last_update_time is None:
-        print("Error: Unable to get last update time.")
-        return
-    
-    generate_warp_config(top_servers, last_update_time)
-    clean_up()
-    print("Warp execution and configuration generation completed successfully.")
-
-if __name__ == "__main__":
-    main()
+        print(
